787e84ced5625b7ee19244489539fa1152ad8216
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-28 14:21:07 -0800

    add character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#tweet-text").on('input', function(){
+    const charCount = $(this).val().length;
+    const reamainChars = 140 - charCount;
+    const countElement = $(this).parent().find('.counter');
+    
+    countElement.text(reamainChars);
+
+    if (reamainChars < 0) {
+      countElement.addClass("negative");
+    } else {
+      countElement.removeClass("negative");
+    }
+  });
+
+});
